67d8e75ae4e282a22a695f31a1610150aa71ecf2
sebastian3o <pap1913barbasebastian@gmail.com>
2023-03-16 00:00:46 +0200

    Adaugare Model pentru client

---
+++ Models/Servicii/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementVinarie.Models.Servicii
+{
+    public class Client
+    {
+        public int ClientId { set; get; }
+        public string Nume { set; get; }
+        public string Prenume { set; get; }
+
+        public DateOnly DataNasterii { set; get;}
+
+        public string Gen { set; get; }
+        public string Telefon { set; get; }
+
+        public string Email { set; get; }
+    }
+}
